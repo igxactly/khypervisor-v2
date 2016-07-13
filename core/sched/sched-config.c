f578f407c97694109a0d0b48be25396e59bdd464
Ingu Kang <igxactly@gmail.com>
2016-07-13 22:15:32 +0900

    Use config!

--- core/sched/sched-config.c
+++ core/sched/sched-config.c
@@ -12,14 +12,21 @@ const struct sched_policy *schedconf_g_policy[NR_CPUS] = {
 uint32_t schedconf_g_vcpu_to_pcpu_map[TOTAL_VCPUS] = {
     0,
     0,
-    0,
+    // 0,
     1
 };
 
+uint32_t schedconf_rm_tick_interval_ms[NR_CPUS] = {
+    1000,
+    0,
+    0,
+    0
+};
+
 uint32_t schedconf_rm_period_budget[TOTAL_VCPUS][2] = {
-    {10, 2},
-    {10, 2},
-    {10, 2},
+    {10, 5},
+    {10, 5},
+    // {10, 2},
     {0, }
 };
 
@@ -27,7 +34,7 @@ uint32_t schedconf_rm_period_budget[TOTAL_VCPUS][2] = {
 uint32_t schedconf_rr_slice[TOTAL_VCPUS] = {
     0,
     0,
-    0,
+    // 0,
     5
 };
 */
--- include/core/sched/sched-config.h
+++ include/core/sched/sched-config.h
@@ -3,13 +3,16 @@
 
 #include <core/sched/scheduler_skeleton.h>
 
-#define TOTAL_VCPUS 4
+#define TOTAL_VCPUS NUM_GUESTS_STATIC
 
 extern const struct sched_policy *
     schedconf_g_policy[NR_CPUS];
 
 extern uint32_t
     schedconf_g_vcpu_to_pcpu_map[TOTAL_VCPUS];
+
+extern uint32_t
+    schedconf_rm_tick_interval_ms[NR_CPUS];
 
 extern uint32_t
     schedconf_rm_period_budget[TOTAL_VCPUS][2];
